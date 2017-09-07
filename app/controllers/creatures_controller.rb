021dd04983c2b15168c55db8ea7d56dd483b29bd
EMENGH <eamendez62@gmail.com>
2017-09-07 13:28:55 -0400

    added creatures, view, model

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,12 @@
+#
+# app/controllers/creatures_controller.rb
+#
+class CreaturesController < ApplicationController
+    # display all creatures
+      def index
+        # get all creatures from db and save to instance variable
+        @creatures = Creature.all
+        # render the index view (it has access to instance variable)
+        # remember the default behavior is to render :index
+      end
+    end
